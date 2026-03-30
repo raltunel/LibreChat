98d120a66dd0b234202cee8d15099ed169d67480
Ridvan Altunel <raltunel@proven.com.tr>
2026-03-31 00:25:46 +0300

    modify styling

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -57,13 +57,18 @@ function AuthLayout({
   };
 
   return (
-    <div className="relative flex min-h-screen flex-col bg-white dark:bg-gray-900">
+    <div className="auth-layout-bg relative flex min-h-screen flex-col">
       <Banner />
       <BlinkAnimation active={isFetching}>
         <div className="mt-6 h-10 w-full bg-cover">
           <img
             src="assets/logo.svg"
-            className="h-full w-full object-contain"
+            className="hidden h-full w-full object-contain dark:block"
+            alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? 'LibreChat' })}
+          />
+          <img
+            src="assets/logo-colored.svg"
+            className="block h-full w-full object-contain dark:hidden"
             alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? 'LibreChat' })}
           />
         </div>
@@ -74,7 +79,7 @@ function AuthLayout({
       </div>
 
       <main className="flex flex-grow items-center justify-center">
-        <div className="w-authPageWidth overflow-hidden bg-white px-6 py-4 dark:bg-gray-900 sm:max-w-md sm:rounded-lg">
+        <div className="w-authPageWidth overflow-hidden px-6 py-4 sm:max-w-md sm:rounded-lg">
           {!hasStartupConfigError && !isFetching && header && (
             <h1
               className="mb-4 text-center text-3xl font-semibold text-black dark:text-white"
